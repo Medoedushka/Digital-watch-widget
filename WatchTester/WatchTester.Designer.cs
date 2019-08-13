923fc4cb7c31e266d6bf9c55d44103bf2b7ebe75
Medoedushka <knife491@gmail.com>
2019-08-13 13:49:06 +0200

    Р”РѕР±Р°РІСЊС‚Рµ С„Р°Р№Р»С‹ РїСЂРѕРµРєС‚Р°.

---
+++ WatchTester/WatchTester.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WatchTester
+{
+    partial class WatchTester
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WatchTester));
+            this.digitalWatch1 = new Watch.DigitalWatch();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pcb_OnOff = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cms_Settings = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsm_MostTopChange = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрепитьВиджетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cms_NotifyIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_OnOff)).BeginInit();
+            this.cms_Settings.SuspendLayout();
+            this.cms_NotifyIcon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // digitalWatch1
+            // 
+            this.digitalWatch1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.digitalWatch1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.digitalWatch1.DisplayColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.digitalWatch1.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digitalWatch1.Location = new System.Drawing.Point(2, 9);
+            this.digitalWatch1.Margin = new System.Windows.Forms.Padding(4);
+            this.digitalWatch1.Name = "digitalWatch1";
+            this.digitalWatch1.OffColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.digitalWatch1.OnColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(187)))), ((int)(((byte)(199)))));
+            this.digitalWatch1.Size = new System.Drawing.Size(225, 80);
+            this.digitalWatch1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(122, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Created by Medoed";
+            // 
+            // pcb_OnOff
+            // 
+            this.pcb_OnOff.Image = global::WatchTester.Properties.Resources.unpressedButton;
+            this.pcb_OnOff.Location = new System.Drawing.Point(12, 92);
+            this.pcb_OnOff.Name = "pcb_OnOff";
+            this.pcb_OnOff.Size = new System.Drawing.Size(25, 25);
+            this.pcb_OnOff.TabIndex = 4;
+            this.pcb_OnOff.TabStop = false;
+            this.pcb_OnOff.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pcb_OnOff.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ON/OFF";
+            // 
+            // cms_Settings
+            // 
+            this.cms_Settings.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.видToolStripMenuItem,
+            this.закрепитьВиджетToolStripMenuItem});
+            this.cms_Settings.Name = "cms_Settings";
+            this.cms_Settings.Size = new System.Drawing.Size(173, 48);
+            // 
+            // видToolStripMenuItem
+            // 
+            this.видToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.видToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsm_MostTopChange});
+            this.видToolStripMenuItem.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.видToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.видToolStripMenuItem.Text = "Вид";
+            // 
+            // tsm_MostTopChange
+            // 
+            this.tsm_MostTopChange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.tsm_MostTopChange.Checked = true;
+            this.tsm_MostTopChange.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsm_MostTopChange.ForeColor = System.Drawing.Color.White;
+            this.tsm_MostTopChange.Name = "tsm_MostTopChange";
+            this.tsm_MostTopChange.Size = new System.Drawing.Size(180, 22);
+            this.tsm_MostTopChange.Text = "Поверх других окон";
+            this.tsm_MostTopChange.Click += new System.EventHandler(this.tsm_MostTopChange_Click);
+            // 
+            // закрепитьВиджетToolStripMenuItem
+            // 
+            this.закрепитьВиджетToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.закрепитьВиджетToolStripMenuItem.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.закрепитьВиджетToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.закрепитьВиджетToolStripMenuItem.Name = "закрепитьВиджетToolStripMenuItem";
+            this.закрепитьВиджетToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.закрепитьВиджетToolStripMenuItem.Text = "Закрепить виджет";
+            this.закрепитьВиджетToolStripMenuItem.Click += new System.EventHandler(this.закрепитьВиджетToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.cms_NotifyIcon;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "DigitalWatch";
+            this.notifyIcon1.Visible = true;
+            // 
+            // cms_NotifyIcon
+            // 
+            this.cms_NotifyIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.cms_NotifyIcon.Name = "cms_NotifyIcon";
+            this.cms_NotifyIcon.Size = new System.Drawing.Size(109, 26);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // WatchTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(231, 137);
+            this.ContextMenuStrip = this.cms_Settings;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pcb_OnOff);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.digitalWatch1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(1280, 40);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WatchTester";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "WatchTester";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.WatchTester_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.WatchTester_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_OnOff)).EndInit();
+            this.cms_Settings.ResumeLayout(false);
+            this.cms_NotifyIcon.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Watch.DigitalWatch digitalWatch1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pcb_OnOff;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip cms_Settings;
+        private System.Windows.Forms.ToolStripMenuItem видToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsm_MostTopChange;
+        private System.Windows.Forms.ToolStripMenuItem закрепитьВиджетToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip cms_NotifyIcon;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ WatchTester/WatchTester.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Reflection;
+using System.Drawing;
+using System.Windows.Forms;
+using MyClassLibrary;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace WatchTester
+{
+    public partial class WatchTester : Form
+    {
+        bool ActiveWatch = true;
+        string configFile = @"..\..\Resources\Config.txt";
+        ConfigControler configControler;
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST) m.Result = (IntPtr)(HT_CAPTION);
+        }
+        
+
+        public WatchTester()
+        {
+            InitializeComponent();
+            configControler = new ConfigControler(configFile);
+            
+        }
+        
+        // Включение/выключение часов.
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pcb_OnOff.Image = Properties.Resources.pressedButton;
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pcb_OnOff.Image = Properties.Resources.unpressedButton;
+            if (!ActiveWatch)
+                digitalWatch1.WatchOn();
+            else digitalWatch1.WatchOff();
+            ActiveWatch = !ActiveWatch;
+        }
+
+        private void WatchTester_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) cms_Settings.Show();
+        }
+
+        private void tsm_MostTopChange_Click(object sender, EventArgs e)
+        {
+            if (!tsm_MostTopChange.Checked) this.TopMost = true;
+            else this.TopMost = false;
+            tsm_MostTopChange.Checked = !tsm_MostTopChange.Checked;
+            
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void WatchTester_Load(object sender, EventArgs e)
+        {
+            SetAutoRunValue(true, Assembly.GetExecutingAssembly().Location);
+            string[] el = configControler.GetConfig("Watch Location").Split('X');
+            this.Location = new Point(int.Parse(el[0]), int.Parse(el[1]));
+            digitalWatch1.WatchOn();
+        }
+
+        private void закрепитьВиджетToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string newPos = this.Location.X + "X" + this.Location.Y;
+            configControler.SetConfig("Watch Location=" + newPos);
+        }
+
+
+        // Работа с реестром для добавления приложения в автозагрузку.
+        private bool SetAutoRunValue(bool autorun, string path)
+        {
+            const string name = "DigitalWatch";
+            string exePath = path;
+            RegistryKey reg;
+
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+            try
+            {
+                if (autorun)
+                {
+                    reg.SetValue(name, exePath);
+                }
+                else
+                {
+                    reg.DeleteValue(name);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                reg.Flush();
+                reg.Close();
+            }
+            return true;
+        }
+    }
+}
